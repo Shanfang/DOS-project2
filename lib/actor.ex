1aa416c0035795904e664976efbaa9e04f044ae7
Shanfang <shanfangzhao@gmail.com>
2017-10-02 22:16:38 -0400

    Both actor and coordinator uses GenServer
    Implementation not finished yet

--- lib/actor.ex
+++ lib/actor.ex
@@ -1,90 +1,193 @@
 defmodule Actor do
-    msg_received = 0
-    s_value = 0
-    w_value = 1
-    pre_ratio = 0
-    id = 0
-    neighbors = []
-    topology = ""
-    algorithm = ""
-    def initialize_actor(num_of_nodes, topo, ID) do
-        s_value = id
-        id = ID
-        topology = topo
+    use GenServer
+    @name ACTOR
+
+    ######################### client API ####################
+    def start_link(counter, s_value, w_value) do
+        GenServer.start_link(__MODULE__, [counter, s_value, w_value], opts ++ [name: ACTOR)
     end
 
-    defp handle_msg do
-        receive do
-            # start of gossip protocol
-            {:start_gossip, actors} ->
-                algorithm = "gossip"
-                neighbors = actors
-                msg_received = msg_received + 1
-                send self, :tell_neighbor                   
-                
-                if msg_received == 1 do
-                    send coordinator, :gossip_converge
-                end
+    def start_work([num_of_nodes, topology, algorithm, id]) do
+        GenServer.call(@name, {:start_work, [num_of_nodes, topology, algorithm, id]})
+    end
 
-                if msg_received == 10 do
-                    Process.exit(self, :kill)                    
-                end 
+    def get_rumor(:rumor) do
+        GenServer.call(@name, :get_rumor)
+    end
+    def push_sum_rumor([delta_s, delta_w]) do
+        GenServer.call(@name, {:push_sum_rumor, [delta_s, delta_w]})
+    end
+    ######################### callbacks ####################
 
-            {:gossip_neighbor} ->
-                # propograte the rumor
+    def init([counter, s_value, w_value]) do
+        {:ok, [counter : 0, s_value: 0, w_value: 1]}
+    end
+
+    # send rumor to its neighbors, choose neighbor according to topo config
+    def handle_call({:start_work, [num_of_nodes, topology, algorithm, id]}, _from, state) do
+        case algorithm do
+            "gossip" -> 
                 case topology do
                     "full" ->
                         neighbor_id = neighbor_full(id, num_of_nodes)
-                        propgrate_gossip(neighbors, neighbor_id)
+                        propagate_gossip(neighbors, neighbor_id)
                     "2D" ->
                         neighbor_id = neighbor_2D(id, num_of_nodes)
-                        propgrate_gossip(neighbors, neighbor_id)                        
+                        propagate_gossip(neighbors, neighbor_id)                        
                     "line" ->
                         neighbor_id = neighbor_line(id, num_of_nodes)
-                        propgrate_gossip(neighbors, neighbor_id)                        
+                        propagate_gossip(neighbors, neighbor_id)                        
                     "imp2D" ->
                         neighbor_id = neighbor_imp2D(id, num_of_nodes)
-                        propgrate_gossip(neighbors, neighbor_id)                        
+                        propagate_gossip(neighbors, neighbor_id)        
+                    _ ->
+                        IO.puts "Invalid topology, please try again!"
                 end
-            # end of gossip protocol
-
-            # start of push sum protocol
-            {:start_push_sum, actors} ->
-                algorith = "push_sum"
-                neighbors = actors
-            {:push_sum_neighbor} ->
-                # propograte the rumor
+                new_state = [counter: 1, s_value: 0, w_value: 1]
+            "push_sum" ->
                 case topology do
                     "full" ->
                         neighbor_id = neighbor_full(id, num_of_nodes)
-                        propgrate_push_sum(neighbors, neighbor_id)
+                        propagate_push_sum(neighbors, neighbor_id)
                     "2D" ->
                         neighbor_id = neighbor_2D(id, num_of_nodes)
-                        propgrate_push_sum(neighbors, neighbor_id)                        
+                        propagate_push_sum(neighbors, neighbor_id)                        
                     "line" ->
                         neighbor_id = neighbor_line(id, num_of_nodes)
-                        propgrate_push_sum(neighbors, neighbor_id)                        
+                        propagate_push_sum(neighbors, neighbor_id)                        
                     "imp2D" ->
                         neighbor_id = neighbor_imp2D(id, num_of_nodes)
-                        propgrate_push_sum(neighbors, neighbor_id)                        
+                        propagate_push_sum(neighbors, neighbor_id) 
+                    _ ->
+                        IO.puts "Invalid topology, please try again!"
                 end
-
+                new_state = [counter: 1, s_value: id, w_value: 1]
+            _ ->
+                IO.puts "Invalid algorithm, please try again!"
+                #{:error}
         end
-        handle_msg
+        {:ok, new_state}
     end
 
-    defp propgrate_gossip(neighbors, neighbor_id) do
+    # process rumor for the push_sum algorithm
+    def handle_cast({:get_rumor,[delta_s, delta_w]}, state) do
+
+    ########## s_value, w_value, previous_ration, unchange_times are all in the state ###########
+        previous_ration = s_value / w_value
+        s_value = s_value + delta_s
+        w_value = w_value + delta_w
+        currentValue = s_value / w_value
+        if check_unchange(current_ration, previous_ration, unchange_times) == 3 do
+            #send coordinator, :push_sum_converge
+            Coordinator.converged(:converged)
+            Process.exit(self, :kill)                    
+        end 
+        {:noreply, _state}
+    end 
+
+    # process rumor for the gossip algorithm, and if a actor gets 10 messages, it will be killed
+    def handle_cast({:get_rumor}, state) do
+        new_counter = counter + 1
+        if new_counter == 10 do
+            Coordinator.converged(:converged)
+            #send coordinator, :gossip_converge
+            Process.exit(self, :kill)                    
+        end
+        {:noreply, _state}
+    end 
+
+    ################## helper functions ###################
+
+    # find neighbor for full topology
+    defp neighbor_full(id, num_of_nodes) do
+        neighbors = []
+        for i <- 1..num_of_nodes do
+            neighbors = [i | neighbors]
+        end
+        Enum.filter(neighbors, fn(x) -> x != id end)
+        neighbors
+    end
+
+    # find neighbor for 2D topology
+    defp neighbor_2D(id, num_of_nodes) do
+        neighbors = []
+        square_len = :math.sqrt(num_of_nodes)
+        row_index = id / square_len
+        col_index = id % square_len
+        if row_index - 1 >= 0 do
+            neighbor_up = (row_index - 1) * square_len + col_index
+            neighbors = [neighbor_up | neighbors]
+        end
+        
+        if row_index + 1 <= square_len - 1 do
+            neighbor_down = (row_index + 1) * square_len + col_index
+            neighbors = [neighbor_down | neighbors]
+        end
+
+        if col_index - 1 >= 0 do
+            neighbor_left = id - 1
+            neighbors = [neighbor_left | neighbors]
+        end
+
+        if col_index + 1 <= square_len - 1 do
+            neighbor_right = id + 1
+            neighbors = [neighbor_right | neighbors]
+        end
+        neighbors
+    end
+
+    # find neighbor for line topology
+    defp neighbor_line(id, num_of_nodes) do
+        neighbors = []
+        case id do
+            num_of_nodes - 1 ->
+                neighbors = [id - 1 | neighbors]
+            0 ->
+                neighbors = [id + 1 | neighbors]
+            _ ->
+                neighbors = [id + 1 | neighbors]
+                neighbors = [id - 1 | neighbors]
+        end
+        neighbors
+    end
+
+    # find neighbor for imperfect 2D topology
+    defp neighbor_imp2D(id, num_of_nodes) do
+        neighbors = neighbor_2D(id, num_of_nodes)
+
+        # add a random neighbor
+        random_neighbor = :rand.uniform(num_of_nodes) - 1
+        neighbors = [random_neighbor | neighbors]
+    end
+
+    # propagate gossip by sending it to neighbors
+    defp propagate_gossip(neighbors, neighbor_id) do
         # neighbor_pid = Enum.find_value(neighbors, fn ({id, pid}) when neighbor_id == id -> pid end)
         neighbor_pid = List.keyfind(neighbors, neighbor_id, 0) |> elem(1)
         if alive?(neighbor_pid) do
-            send neighbor_pid, :start_gossip
+            send neighbor_pid, :rumor
         end    
     end
 
-    defp propgrate_push_sum(neighbors, neighbor_id) do
+    defp propagate_push_sum(neighbors, neighbor_id, s_value, w_value) do
         neighbor_pid = Enum.find_value(neighbors, fn ({id, pid}) when neighbor_id == id -> pid end)
         if alive?(neighbor_pid) do
-            send neighbor_pid, :start_push_sum
+            # neighbors is a map, each os which is a tuple {id, pid_name} 
+            # pid_name is created when initializing an actor
+
+            # fetch the neighbor by its id, then call the get_rumor() of that pid_name
+            # so there is no need to send it a msg directly, the GenServer handles it for us
+            ##########send neighbor_pid, {:push_sum_rumor, [s_value / 2, w_value / 2]}
+
         end    
+    end
+
+    defp check_unchange(current_ration, previous_ration, unchange_times) do
+        if abs(current_ration - previous_ration) < 1e-10 do
+            unchange_times = unchange_times + 1
+        else 
+            unchange_times = 0
+        end
+        unchange_times
     end
 end
--- lib/coordinator.ex
+++ lib/coordinator.ex
@@ -1,6 +1,57 @@
 defmodule Coordinator do
+    use GenServer
+    @name COORDINATOR
 
-    def initialize_actor_system(num_of_nodes, topology, algorithm) do
+    ######################### client API ####################
+    def start_link([counter, num_of_nodes]) do
+        GenServer.start_link(__MODULE__, [counter, num_of_nodes], opts ++ [name: COORDINATOR)
+    end
+
+    def converged(:converged) do
+        GenServer.cast(@name, :converged)
+    end
+
+    ######################### callbacks ####################
+
+    def init([counter, num_of_nodes, topology, algorithm]) do
+        # state = init_state()
+        initialize_actor_system(num_of_nodes, topology, algorithm)
+        {:ok, []}
+    end
+
+    def handle_cast(:converged, state) do
+        conv_count = conv_count + 1
+        if conv_count == num_of_nodes do
+            end_time = :os.system_time(:millisecond)
+            conv_time = end_time - start_time
+            IO.puts "Converged, time taken is: " <> Integer.to_string(conv_time) <> "millseconds"  
+        end
+        {:noreply, _state}
+    end 
+
+    ################## helper functions ####################
+
+    defp initialize_actor_system(num_of_nodes, topology, algorithm) do       
+        # building actors system
+        for id <- 1..num_of_nodes do
+            actor_pid = spawn(Actor, :initialize_actor, [num_of_nodes, topology, i])
+            actor_tuple = {Integer.to_string : actor_pid}
+            neighbors = [actor_tuple | neighbors]
+            #is_alive = [true | is_alive]    
+        end 
+
+        # randomly choose an actor as the starting node 
+        initial_actor = Enum.random(neighbors)
+
+        # start timing when initialization is complete
+        start_time = :os.system_time(:millisecond)
+
+        ############
+
+        #########start the initial_actor ##########
+    end
+
+    defp initialize_actor_system(num_of_nodes, topology, algorithm) do
         
         # count the number of actors 
         # (i) who has received the rumor at least once (in the scenario of gossip algorithm)
@@ -10,9 +61,9 @@ defmodule Coordinator do
         ##############!!!!!!!!!!!!!!! neighbors should be like [one: "first", two: "second"]
         #### then, need to find how to add a key-value pair to the list###########
 
-
-
-
+       # Map.fetch(neighbors, neighbot_id) => this will return {:ok, neighbot_id}
+        # then use elem({:ok, neighbot_id}, 1) to get the pid from the tuple
+# now I am working on the genserver branch
         
         neighbors = [{}]
         # is_alive = []
